d29d88d9aaadf4201cc4ab21351d939724143585
Suresh <venkatasuresh051@yahoo.com>
2026-05-28 14:13:00 +0530

    Revert "Optimize kernel power efficiency and fix post-boot UI stutter"
    This reverts commit f2a3047e8d22702f9cc08077b0237931135c30c7.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -49,7 +49,7 @@ static struct work_struct input_boost_work;
 
 static bool input_boost_enabled;
 
-static unsigned int input_boost_ms = 120;
+static unsigned int input_boost_ms = 40;
 show_one(input_boost_ms);
 store_one(input_boost_ms);
 cpu_boost_attr_rw(input_boost_ms);
